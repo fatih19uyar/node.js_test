3b9d48e1ffcb32f2d142108b3dbc2175f11107bb
omerfatihuyar <Mutlu123>
2023-01-31 15:26:19 +0300

    0.63 31.01.2023 Body-Parser

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use ('/',(req,res,next)=> {
-    console.log("This Always Runs!!");
-    next();
-})
+app.use(bodyParser.urlencoded({extended:false}));
 
 
 app.use('/add-product',(req, res, next) => { 
-    console.log('in another middleware here!');
-    res.send('<h1>Hello from Add Product Page!</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
 app.use('/',(req, res, next) => { 
-    console.log('in another middleware here!');
     res.send('<h1>Hello from Express!</h1>');
 });
 
