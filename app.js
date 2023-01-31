464fdc489396d8633d8af4d84fc003e7c6fc2daa
omerfatihuyar <Mutlu123>
2023-01-31 22:23:51 +0300

    067 31.01.2023 Filtering Paths

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.status(404).send('<h1>Page No Found</h1>');
 })
 
