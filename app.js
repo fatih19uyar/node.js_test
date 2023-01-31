fe8c9329ad001d65eb41f0fc96127fc139c57757
omerfatihuyar <Mutlu123>
2023-01-31 14:58:07 +0300

    063 31.01.2023

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => { 
-    console.log('in the middleware here!');
-    next(); //Allows the request to continue to the next middleware in line 
+app.use ('/',(req,res,next)=> {
+    console.log("This Always Runs!!");
+    next();
+})
+
+
+app.use('/add-product',(req, res, next) => { 
+    console.log('in another middleware here!');
+    res.send('<h1>Hello from Add Product Page!</h1>');
 });
 
-app.use((req, res, next) => { 
+
+app.use('/',(req, res, next) => { 
     console.log('in another middleware here!');
     res.send('<h1>Hello from Express!</h1>');
 });
